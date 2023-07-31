8fa95ec7bc7b6a424654d11b7a8ac265bf83a9bd
imranhosain6554 <imran122.office@gmail.com>
2023-07-31 12:37:45 +0600

    fix cart 2

--- src/fake_api/ProductList.js
+++ src/fake_api/ProductList.js
@@ -1,416 +1,406 @@
 const ProductList = [
-    // > WEDDING BANDS     // > ENGAGEMENT RINGS    // > EARRINGS    // › BRACELETS    // › NECKLACES    // > WATCHES    // › VINTAGE RINGS    // > VINTAGE JEWELLERY
-    // > PENDANTS     // > MARCASITE    // › LOOSE DIAMONDS    // > DESIGNERS
-    {
-        id: "1",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_01.png"
-    },
-    {
-        id: "2",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_02.png"
-    },
-    {
-        id: "3",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_03.png"
-    },
-    {
-        id: "4",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_04.png"
-    },
-    {
-        id: "5",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_05.png"
-    },
-    {
-        id: "6",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_06.png"
-    },
-    {
-        id: "7",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_07.png"
-    },
-    {
-        id: "8",
-        title: 'Diamond Ring',
-        category: "ENGAGEMENT RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_08.png"
-    },
-    {
-        id: "9",
-        title: 'Diamond Ring',
-        category: "ENGAGEMENT RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_09.png"
-    },
-    {
-        id: "10",
-        title: 'Diamond Ring',
-        category: "ENGAGEMENT RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_10.png"
-    },
-    {
-        id: "11",
-        title: 'Diamond Ring',
-        category: "ENGAGEMENT RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_11.png"
-    },
-    {
-        id: "12",
-        title: 'Diamond Ring',
-        category: "ENGAGEMENT RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_12.png"
-    },
-    {
-        id: "13",
-        title: 'Diamond Ring',
-        category: "VINTAGE RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_13.png"
-    },
-    {
-        id: "14",
-        title: 'Diamond Ring',
-        category: "VINTAGE RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_13.png"
-    },
-    {
-        id: "15",
-        title: 'Diamond Ring',
-        category: "VINTAGE RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_14.png"
-    },
-    {
-        id: "16",
-        title: 'Diamond Ring',
-        category: "VINTAGE RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_15.png"
-    },
-    {
-        id: "17",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_16.png"
-    },
-    {
-        id: "18",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_17.png"
-    },
-    {
-        id: "19",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_18.png"
-    },
-    {
-        id: "20",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_19.png"
-    },
+  // > WEDDING BANDS     // > ENGAGEMENT RINGS    // > EARRINGS    // › BRACELETS    // › NECKLACES    // > WATCHES    // › VINTAGE RINGS    // > VINTAGE JEWELLERY
+  // > PENDANTS     // > MARCASITE    // › LOOSE DIAMONDS    // > DESIGNERS
+  {
+    id: "1",
+    title: "Diamond Ring",
+    category: "WEDDING BANDS",
+    price: 960000,
+    img: "../assets/products/jeweleryProduct_01.png",
+  },
+  {
+    id: "2",
+    title: "Diamond Ring",
+    category: "WEDDING BANDS",
+    price: 2000,
+    img: "../assets/products/jeweleryProduct_02.png",
+  },
+  {
+    id: "3",
+    title: "Diamond neckless",
+    category: "WEDDING BANDS",
+    price: 100,
+    img: "../assets/products/jeweleryProduct_03.png",
+  },
+  {
+    id: "4",
+    title: "Diamond Ring",
+    category: "WEDDING BANDS",
+    price: 400,
+    img: "../assets/products/jeweleryProduct_04.png",
+  },
+  {
+    id: "5",
+    title: "Diamond Ring",
+    category: "WEDDING BANDS",
+    price: 9400,
+    img: "../assets/products/jeweleryProduct_05.png",
+  },
+  {
+    id: "6",
+    title: "Diamond Ring",
+    category: "WEDDING BANDS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_06.png",
+  },
+  {
+    id: "7",
+    title: "Diamond Ring",
+    category: "WEDDING BANDS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_07.png",
+  },
+  {
+    id: "8",
+    title: "Diamond Ring",
+    category: "ENGAGEMENT RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_08.png",
+  },
+  {
+    id: "9",
+    title: "Diamond Ring",
+    category: "ENGAGEMENT RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_09.png",
+  },
+  {
+    id: "10",
+    title: "Diamond Ring",
+    category: "ENGAGEMENT RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_10.png",
+  },
+  {
+    id: "11",
+    title: "Diamond Ring",
+    category: "ENGAGEMENT RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_11.png",
+  },
+  {
+    id: "12",
+    title: "Diamond Ring",
+    category: "ENGAGEMENT RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_12.png",
+  },
+  {
+    id: "13",
+    title: "Diamond Ring",
+    category: "VINTAGE RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_13.png",
+  },
+  {
+    id: "14",
+    title: "Diamond Ring",
+    category: "VINTAGE RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_13.png",
+  },
+  {
+    id: "15",
+    title: "Diamond Ring",
+    category: "VINTAGE RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_14.png",
+  },
+  {
+    id: "16",
+    title: "Diamond Ring",
+    category: "VINTAGE RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_15.png",
+  },
+  {
+    id: "17",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_16.png",
+  },
+  {
+    id: "18",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_17.png",
+  },
+  {
+    id: "19",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_18.png",
+  },
+  {
+    id: "20",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_19.png",
+  },
 
-    {
-        id: "21",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_20.png"
-    },
-    {
-        id: "22",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_01.png"
-    },
-    {
-        id: "23",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_02.png"
-    },
+  {
+    id: "21",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_20.png",
+  },
+  {
+    id: "22",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_01.png",
+  },
+  {
+    id: "23",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_02.png",
+  },
 
-    {
-        id: "24",
-        title: 'Diamond Ring',
-        category: "BRACELETS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_03.png"
-    },
+  {
+    id: "24",
+    title: "Diamond Ring",
+    category: "BRACELETS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_03.png",
+  },
 
+  {
+    id: "25",
+    title: "Diamond Ring",
+    category: "NECKLACES",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_04.png",
+  },
+  {
+    id: "26",
+    title: "Diamond Ring",
+    category: "NECKLACES",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_05.png",
+  },
+  {
+    id: "27",
+    title: "Diamond Ring",
+    category: "NECKLACES",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_06.png",
+  },
+  {
+    id: "28",
+    title: "Diamond Ring",
+    category: "MARCASITE",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_07.png",
+  },
+  {
+    id: "29",
+    title: "Diamond Ring",
+    category: "LOOSE DIAMONDS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_08.png",
+  },
+  {
+    id: "30",
+    title: "Diamond Ring",
+    category: "LOOSE DIAMONDS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_09.png",
+  },
+  {
+    id: "31",
+    title: "Diamond Ring",
+    category: "WEDDING BANDS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_10.png",
+  },
+  {
+    id: "32",
+    title: "Diamond Ring",
+    category: "WATCHES",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_11.png",
+  },
+  {
+    id: "33",
+    title: "Diamond Ring",
+    category: "WATCHES",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_12.png",
+  },
+  {
+    id: "34",
+    title: "Diamond Ring",
+    category: "WATCHES",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_13.png",
+  },
+  {
+    id: "35",
+    title: "Diamond Ring",
+    category: "WATCHES",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_14.png",
+  },
+  {
+    id: "36",
+    title: "Diamond Ring",
+    category: "ENGAGEMENT RINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_15.png",
+  },
+  {
+    id: "37",
+    title: "Diamond Ring",
+    category: "EARRINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_16.png",
+  },
+  {
+    id: "38",
+    title: "Diamond Ring",
+    category: "EARRINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_17.png",
+  },
+  {
+    id: "39",
+    title: "Diamond Ring",
+    category: "EARRINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_18.png",
+  },
+  {
+    id: "40",
+    title: "Diamond Ring",
+    category: "EARRINGS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_19.png",
+  },
+  {
+    id: "41",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_19.png",
+  },
+  {
+    id: "42",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_20.png",
+  },
+  {
+    id: "43",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_01.png",
+  },
+  {
+    id: "44",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_02.png",
+  },
 
+  {
+    id: "45",
+    title: "Diamond Ring",
+    category: "PENDANTS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_03.png",
+  },
+  {
+    id: "46",
+    title: "Diamond Ring",
+    category: "PENDANTS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_04.png",
+  },
+  {
+    id: "47",
+    title: "Diamond Ring",
+    category: "PENDANTS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_05.png",
+  },
 
+  {
+    id: "48",
+    title: "Diamond Ring",
+    category: "DESIGNERS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_06.png",
+  },
 
-    {
-        id: "25",
-        title: 'Diamond Ring',
-        category: "NECKLACES",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_04.png"
-    },
-    {
-        id: "26",
-        title: 'Diamond Ring',
-        category: "NECKLACES",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_05.png"
-    },
-    {
-        id: "27",
-        title: 'Diamond Ring',
-        category: "NECKLACES",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_06.png"
-    },
-    {
-        id: "28",
-        title: 'Diamond Ring',
-        category: "MARCASITE",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_07.png"
-    },
-    {
-        id: "29",
-        title: 'Diamond Ring',
-        category: "LOOSE DIAMONDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_08.png"
-    },
-    {
-        id: "30",
-        title: 'Diamond Ring',
-        category: "LOOSE DIAMONDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_09.png"
-    },
-    {
-        id: "31",
-        title: 'Diamond Ring',
-        category: "WEDDING BANDS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_10.png"
-    },
-    {
-        id: "32",
-        title: 'Diamond Ring',
-        category: "WATCHES",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_11.png"
-    },
-    {
-        id: "33",
-        title: 'Diamond Ring',
-        category: "WATCHES",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_12.png"
-    },
-    {
-        id: "34",
-        title: 'Diamond Ring',
-        category: "WATCHES",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_13.png"
-    },
-    {
-        id: "35",
-        title: 'Diamond Ring',
-        category: "WATCHES",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_14.png"
-    },
-    {
-        id: "36",
-        title: 'Diamond Ring',
-        category: "ENGAGEMENT RINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_15.png"
-    },
-    {
-        id: "37",
-        title: 'Diamond Ring',
-        category: "EARRINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_16.png"
-    },
-    {
-        id: "38",
-        title: 'Diamond Ring',
-        category: "EARRINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_17.png"
-    },
-    {
-        id: "39",
-        title: 'Diamond Ring',
-        category: "EARRINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_18.png"
-    },
-    {
-        id: "40",
-        title: 'Diamond Ring',
-        category: "EARRINGS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_19.png"
-    },
-    {
-        id: "41",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_19.png"
-    },
-    {
-        id: "42",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_20.png"
-    },
-    {
-        id: "43",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_01.png"
-    },
-    {
-        id: "44",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_02.png"
-    },
+  {
+    id: "49",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_08.png",
+  },
+  {
+    id: "50",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_07.png",
+  },
+  {
+    id: "51",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_09.png",
+  },
+  {
+    id: "52",
+    title: "Diamond Ring",
+    category: "VINTAGE JEWELLERY",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_10.png",
+  },
 
-    {
-        id: "45",
-        title: 'Diamond Ring',
-        category: "PENDANTS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_03.png"
-    },
-    {
-        id: "46",
-        title: 'Diamond Ring',
-        category: "PENDANTS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_04.png"
-    },
-    {
-        id: "47",
-        title: 'Diamond Ring',
-        category: "PENDANTS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_05.png"
-    },
+  {
+    id: "53",
+    title: "Diamond Ring",
+    category: "DESIGNERS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_11.png",
+  },
+  {
+    id: "54",
+    title: "Diamond Ring",
+    category: "DESIGNERS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_12.png",
+  },
+  {
+    id: "55",
+    title: "Diamond Ring",
+    category: "DESIGNERS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_13.png",
+  },
 
-    {
-        id: "48",
-        title: 'Diamond Ring',
-        category: "DESIGNERS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_06.png"
-    },
-
-
-
-
-    {
-        id: "49",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_08.png"
-    },
-    {
-        id: "50",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_07.png"
-    },
-    {
-        id: "51",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_09.png"
-    },
-    {
-        id: "52",
-        title: 'Diamond Ring',
-        category: "VINTAGE JEWELLERY",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_10.png"
-    },
-
-    {
-        id: "53",
-        title: 'Diamond Ring',
-        category: "DESIGNERS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_11.png"
-    },
-    {
-        id: "54",
-        title: 'Diamond Ring',
-        category: "DESIGNERS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_12.png"
-    },
-    {
-        id: "55",
-        title: 'Diamond Ring',
-        category: "DESIGNERS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_13.png"
-    },
-
-    {
-        id: "56",
-        title: 'Diamond Ring',
-        category: "DESIGNERS",
-        price: '৳ 90000',
-        img: "../assets/products/jeweleryProduct_14.png"
-    },
-
-
-
-
+  {
+    id: "56",
+    title: "Diamond Ring",
+    category: "DESIGNERS",
+    price: 90000,
+    img: "../assets/products/jeweleryProduct_14.png",
+  },
 ];
 
 export default ProductList;
